c8f93e36da6418781ad510de3d9ab6fbd317b602
Gabby <bruck.g@husky.neu.edu>
2020-10-10 00:55:32 -0400

    first commit

---
+++ src/test/java/com/example/cs3200fall2020serverjavagbruck/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cs3200fall2020serverjavagbruck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
